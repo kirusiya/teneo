0ec94fca02a8a8c500384e56a14fb2f7dac11fe1
kirusiya <edward.avalos.severiche@gmail.com>
2017-01-24 03:58:56 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'teneo',
+		wp_ws_url : 'http://teneo.ajamba.org/wp-appkit-api/teneo',
+		wp_url : 'http://teneo.ajamba.org',
+		theme : 'q-ios',
+		version : '1.0',
+		app_title : '',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : ',7Y71N#<pr$%sfGR;0%g|f{.E$`i={u]ne_J[/K1>*&,@*)Yw1R2gY1s+eKi]bf[',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
